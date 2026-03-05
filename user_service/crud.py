ce4e19c858d54834c67f98854a1f703aec8a624d
Abdul Hadi <abdulhadibaig37@gmail.com>
2026-03-06 02:26:18 +0500

    Add validation and exception handling to User Service

--- user_service/crud.py
+++ user_service/crud.py
@@ -6,6 +6,10 @@ async def get_users(db: AsyncSession):
     result = await db.execute(select(models.User))
     return result.scalars().all()
 
+async def get_user_by_email(db: AsyncSession, email: str):
+    result = await db.execute(select(models.User).where(models.User.email == email))
+    return result.scalars().first()
+
 async def create_user(db: AsyncSession, user: schemas.UserCreate):
     db_user = models.User(name=user.name, email=user.email)
     db.add(db_user)
--- user_service/main.py
+++ user_service/main.py
@@ -19,4 +19,7 @@ async def read_users(db: AsyncSession = Depends(database.get_db)):
 
 @app.post("/users", response_model=schemas.UserOut)
 async def create_user(user: schemas.UserCreate, db: AsyncSession = Depends(database.get_db)):
+    db_user = await crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
     return await crud.create_user(db=db, user=user)
